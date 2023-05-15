429cb09b7deb23712d800eda6061ba076043c4ab
= <=>
2023-05-15 12:27:01 -0600

    vite / tailwind / alpine

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    './*.js',
+  ],
+  prefix: 'tw-',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
